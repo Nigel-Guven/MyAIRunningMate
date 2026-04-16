a6b5cc05eb778de45bdd038910992b9da51aed94
Nigel Guven <nigelguven@gmail.com>
2026-04-16 20:52:38 +0100

    Remove postgres service

--- app/backend/running_mate_database_service/database/session.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import settings
-
-engine = create_engine(settings.database_url, echo=True)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
