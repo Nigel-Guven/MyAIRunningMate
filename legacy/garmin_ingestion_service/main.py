8ae7ea1890157d57c89c0c98614fa426cb863628
Nigel Guven <nigelguven@gmail.com>
2026-04-22 16:26:09 +0100

    move garmin service to legacy folder
    Garmin restrictions make the current format for retrieving .fit files obsolete

--- legacy/garmin_ingestion_service/main.py
+++ legacy/garmin_ingestion_service/main.py
@@ -5,21 +5,21 @@ import time
 from garminconnect import Garmin
 from application.garmin_persist import sync_activity
 from application.garmin_fit_cleaner import format_file
-import config
 from application import garmin_connect_ingest
 
 
 def main():
     
-    if not config.EMAIL or not config.PASSWORD:
-        raise ValueError("Missing GARMIN_EMAIL or GARMIN_PASSWORD")
+    #if not config.EMAIL or not config.PASSWORD:
+    #    raise ValueError("Missing GARMIN_EMAIL or GARMIN_PASSWORD")
     
     #client = Garmin(config.EMAIL, config.PASSWORD)
     #client.login()
     
     #config.DOWNLOAD_DIR.mkdir(exist_ok=True)
     
-    #garmin_connect_ingest.download_all_activities(client, config.DOWNLOAD_DIR)
+    #garmin_connect_ingest.download_latest_activity(client, config.DOWNLOAD_DIR)
+    
     BASE_DIR = Path(__file__).resolve().parents[3]
     DOWNLOAD_DIR = BASE_DIR / "data" / "garmin_activities"
     
@@ -28,5 +28,5 @@ def main():
         print(activity)
 
 if __name__ == "__main__":
-    while True:
         main()
+        exit()
